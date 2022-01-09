60b264e79ad555bb10aa6cb17ef5626e58ba3611
anton.bashirov <anton.sh.local@gmail.com>
2022-01-09 15:58:07 +0300

    ART

--- src/main/java/io/art/fibers/Fibers.java
+++ src/main/java/io/art/fibers/Fibers.java
@@ -5,9 +5,13 @@ import static io.art.fibers.service.FiberService.*;
 public class Fibers {
     public static void main(String[] args) {
         createFiber(Fibers::coroutine);
+        System.out.println("suspend");
+        suspend();
     }
 
     public static void coroutine() {
+        suspend();
+        System.out.println("resume");
         System.out.println("Done");
     }
 }
