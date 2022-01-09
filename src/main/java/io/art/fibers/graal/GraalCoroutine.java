6fd1b328e41da710f16bfadffb707404d6bec017
Anton Bashirov <anton.bashirov@rt.ru>
2022-01-09 06:52:35 +0300

    ART

--- src/main/java/io/art/fibers/graal/GraalCoroutine.java
+++ src/main/java/io/art/fibers/graal/GraalCoroutine.java
@@ -10,7 +10,7 @@ import org.graalvm.nativeimage.c.function.*;
 import org.graalvm.nativeimage.c.struct.*;
 import org.graalvm.nativeimage.c.type.*;
 import org.graalvm.word.*;
-import static io.art.core.graal.GraalNativeDirective.SingleLibrary.*;
+import static io.art.core.graal.GraalNativeDirective.GraalSingleLibrary.*;
 import static io.art.fibers.constants.FiberConstants.GraalConstants.*;
 import java.util.*;
 
