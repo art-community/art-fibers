5af4274c8d772f8007cef9ba693a165ae83bb512
anton.bashirov <anton.sh.local@gmail.com>
2022-02-19 04:18:11 +0300

    ART

--- fibers/src/main/java/io/art/fibers/graal/GraalCoroutineFeature.java
+++ fibers/src/main/java/io/art/fibers/graal/GraalCoroutineFeature.java
@@ -1,16 +1,14 @@
 package io.art.fibers.graal;
 
 import com.oracle.svm.core.annotate.*;
-import com.oracle.svm.hosted.*;
-import com.oracle.svm.hosted.c.*;
 import org.graalvm.nativeimage.hosted.*;
+import static io.art.core.graal.GraalNativeRegistrator.*;
 import static io.art.fibers.constants.FiberConstants.GraalConstants.*;
 
 @AutomaticFeature
 public class GraalCoroutineFeature implements Feature {
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
-        NativeLibraries nativeLibraries = ((FeatureImpl.BeforeAnalysisAccessImpl) access).getNativeLibraries();
-        nativeLibraries.addStaticNonJniLibrary(coroutineLibraryFileName());
+        registerStaticNonJniLibrary(access, coroutineLibraryFileName());
     }
 }
